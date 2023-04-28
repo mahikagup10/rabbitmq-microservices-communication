55bdab219a318e150e369a8c302994c7cc2229cf
Kanumuri Sri Charan <74090239+SriCharanK2002@users.noreply.github.com>
2023-04-28 12:06:37 +0530

    final code for consumer_one

--- consumer_one/healthcheck.py
+++ consumer_one/healthcheck.py
@@ -1,55 +1,20 @@
 import pika
-import pymongo
-import json
-import requests
 import time
 
-time.sleep(30)  
+sleepTime = 20
+time.sleep(sleepTime)
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbitmq'))
-
+print('Connecting to server ...')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
+channel.queue_declare(queue='health_check', durable=True)
 
-channel.queue_declare(queue='health_check')
-
-
-client = pymongo.MongoClient('mongodb://mongo:27017/')
-db = client['student_db']
-collection = db['students']
-
-
-def health_check(ch, method, properties, body):
-    # Publish a message to the health-check-queue
-    # channel = connection.channel()
-    print(f"Received message: {body.decode()} from health_check queue")
-    response = requests.get(body.decode())
-    print(f"Response status code: {response.status_code}")
+def callback(ch, method, properties, body):
+    print("Health Check ACK ")
     ch.basic_ack(delivery_tag=method.delivery_tag)
-    channel.basic_publish(exchange='microservices-exchange', routing_key='health_check', body='RabbitMQ is healthy!')
-    connection.close()
-    return 'OK'
+    return "Health check ACK"
 
-
-
-
-# import pika
-
-
-# connection = pika.BlockingConnection(
-#     pika.ConnectionParameters(host='rabbitmq'))
-# channel = connection.channel()
-
-# channel.queue_declare(queue='health_check', durable=True)
-
-# def callback(ch, method, properties, body):
-#     print(f"Received message: {body.decode()} from health_check queue")
-#     response = requests.get(body.decode())
-#     print(f"Response status code: {response.status_code}")
-#     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='health_check', on_message_callback=health_check)
-
-print('Waiting for messages...')
+channel.basic_consume(queue='health_check', on_message_callback=callback)
 channel.start_consuming()
